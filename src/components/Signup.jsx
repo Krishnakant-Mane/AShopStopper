42641b8af4e54c1648aa0fb5241fda5f7e10d012
Krishnakant Mane <krishnamane712c@gmail.com>
2026-01-03 16:58:38 +0530

    Second

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import {useNavigate} from 'react-router-dom'
+import { Link } from "react-router-dom";
+
 
 export const Signup = () => {
 
